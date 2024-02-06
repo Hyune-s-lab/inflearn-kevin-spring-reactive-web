1f70e33f913ef131c997b9d609f9f4c2967baf50
Hyune-c <choi8608@gmail.com>
2024-02-07 03:08:31 +0900

    [Cold Sequence와 Hot Sequence] Cold Sequence와 Hot Sequence Example 코드 설명

---
+++ src/main/kotlin/com/example/inflearnkevinspringreactiveweb/section04/class01/ColdSequenceExample.kt
@@ -0,0 +1,19 @@
+package com.example.inflearnkevinspringreactiveweb.section04.class01
+
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
+import java.util.*
+
+object ColdSequenceExample {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val coldFlux = Flux.fromIterable(mutableListOf("RED", "YELLOW", "PINK"))
+            .map { obj -> obj.lowercase(Locale.getDefault()) }
+
+        coldFlux.subscribe { country -> log.info("### Subscriber1: {}", country) }
+        log.info("###-------------------------")
+        coldFlux.subscribe { country -> log.info("### Subscriber2: {}", country) }
+    }
+}
