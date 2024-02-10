e7bc1170a143ad325b33fd2f75685bd9fbbbaaaf
Hyune-c <choi8608@gmail.com>
2024-02-11 04:04:08 +0900

    [Debugging] Debug 모드를 사용한 Debugging

---
+++ src/main/kotlin/com/example/inflearnkevinspringreactiveweb/section09/class01/DebugModeExample02.kt
@@ -0,0 +1,20 @@
+package com.example.inflearnkevinspringreactiveweb.section09.class01
+
+import com.example.inflearnkevinspringreactiveweb.util.Logger
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Hooks
+
+/**
+ * onOperatorDebug() Hook 메서드를 이용한 Debug mode
+ * - 애플리케이션 전체에서 global 하게 동작한다.
+ */
+object DebugModeExample02 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Hooks.onOperatorDebug()
+
+        Flux.just(2, 4, 6, 8)
+            .zipWith(Flux.just(1, 2, 3, 0)) { x, y -> x / y }
+            .subscribe(Logger::onNext, Logger::onError)
+    }
+}
---
+++ src/main/kotlin/com/example/inflearnkevinspringreactiveweb/section09/class01/DebugModeExample03.kt
@@ -0,0 +1,28 @@
+package com.example.inflearnkevinspringreactiveweb.section09.class01
+
+import com.example.inflearnkevinspringreactiveweb.util.Logger
+import reactor.core.publisher.Flux
+import java.util.*
+
+/**
+ * Non-Debug mode
+ */
+object DebugModeExample03 {
+    private var fruits: MutableMap<String, String> = HashMap()
+
+    init {
+        fruits["banana"] = "바나나"
+        fruits["apple"] = "사과"
+        fruits["pear"] = "배"
+        fruits["grape"] = "포도"
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Flux.fromArray(arrayOf("BANANAS", "APPLES", "PEARS", "MELONS"))
+            .map { obj -> obj.lowercase(Locale.getDefault()) }
+            .map { fruit -> fruit.substring(0, fruit.length - 1) }
+            .map<String> { key -> fruits[key] }
+            .subscribe(Logger::onNext, Logger::onError)
+    }
+}
